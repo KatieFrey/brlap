744715323ff046b4e80f56f95fb60413a3f4a325
sdhar86 <dhar.saurav@gmail.com>
2016-09-12 21:40:59 -0400

    first commit

---
+++ send.php
@@ -0,0 +1,19 @@
+<?php
+
+$usermail = $_POST['email'];
+
+$address = "__YOUR_EMAIL__";
+
+$subject  = "__SUBJECT__";
+
+$headers  = "From: " . strip_tags($usermail) . "\r\n";
+$headers .= "Reply-To: ". strip_tags($usermail) . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-type:text/plain; Charset=UTF-8 \r\n";
+
+$message = "E-mail: ".$usermail."\n";
+
+if(@mail($address, $subject, $message, $headers)) { echo "true"; } 
+else { echo "false"; }
+
+?>
